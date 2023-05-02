2f0ae965ab11417da29612972457374300e62f29
jrwesch <jrwesch@gmail.com>
2023-05-02 13:50:37 -0400

    trying to get paths work for weather api, styled css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-const apiKey = "8eac768920bc228ecc692f30b2371da9"
+const apiKey = "8eac768920bc228ecc692f30b2371da9";
+let cityName;
 
-var searchButton = document.getElementById("searchBtn");
-searchButton.addEventListener("click", function () {
-    console.log("search button clicked");
-    let cityName = $("#cityInput").val();
-    console.log(cityName);
+
+
+    let fetchWeather = function (cityName) {
     let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + apiKey;
     fetch(weatherAPI)
         .then(function (response) {
@@ -18,35 +17,54 @@ searchButton.addEventListener("click", function () {
             // Use 'querySelector' to get the ID of where the Search for a City will be displayed
             var responseContainerEl = document.querySelector("#response-container");
             console.log(data)
+            getCityInfo(data.city.coor.lat, data.city.coord.lon);
         })
         .catch(err => console.log(err));
-})
+};
+
+    var searchButton = document.getElementById("searchBtn");
+    searchButton.addEventListener("click", function () {
+        cityName = $("#cityInput").val();
+        fetchWeather(cityName);
+    })
 
 // get the preset buttons to return weather information
 var presetCityButtons = document.querySelectorAll(".cityNames");
 presetCityButtons.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
-        console.log(document.querySelectorAll(".cityNames"));
-        let cityName = e.target.innerText;
-        console.log(cityName);
-        let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + apiKey;
-        fetch(weatherAPI)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                if (data.cod !== "200") {
-                    console.log("City not found. Please try again");
-                    return;
-                }
-                // Use 'querySelector' to get the ID of where the Search for a City will be displayed
-                var responseContainerEl = document.querySelector("#response-container");
-                console.log(data)
-            })
-            .catch(err => console.log(err));
-    });
+
+        //console.log(document.querySelectorAll(".cityNames"));
+        cityName = e.target.innerText;
+        fetchWeather(cityName);
+        });
 });
 
+    // create container for city and all weather data
+        let getCityInfo = function (lat, lon) {
+            let uvAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + '&lon=' + lon + "&appid=8eac768920bc228ecc692f30b2371da9";
+            fetch(uvApi)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            $('.cityDate').html(cityName + " (" + toDateTime(data.current.dt) + ")" + `<img src="https://openweathermap.org/img/w/${data.current.weather[0].icon}.png" />`); // in the city variable
+            $('.temperature').text("Temp: " + data.current.temp);
+            $('.wind').text("Wind: " + data.current.wind_speed + " MPH");
+            $('.humidity').text("Humidity: " + data.current.humidity + " %");
+            $('.uvIndex').html("UV Index: " + `<span class="btnColor">${data.current.uvi}</span>`);
+            fiveDayForecast(data);
+
+          
+    });
+};
 
 // still need to create a container that contains the city, date, temp, wind, humidity and UV index
-var jsonData;
+    let fiveDayForecast = function (data) {
+        for (let i = 0; i < 5; i++) {
+            console.log(fiveDayForecast);
+            $('.fiveDayForecast').append(fiveDayForecast[i]);
+            $('.dailyForecast').append("Temp: " + data.daily[i].temp);
+            $('.dailyForecast').append("Wind: " + data.daily[i].wind_speed + "MPH");
+            $('.dailyForecast').append("Humidity: " + data.daily[i].humidity + " %");
+            fiveDayForecast = "";
+        };
+    }
